5f0723740c8f920ea1444775b90bb0e08abe44da
harshadrane67 <harshadrane67@gmail.com>
2022-01-14 20:06:29 +0530

    allowed host setting change to *

--- project_clean/settings.py
+++ project_clean/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-metihnura(x1lxt#7dq5zej6f@zz))(w4%511uzlgt8ai$rfiw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
