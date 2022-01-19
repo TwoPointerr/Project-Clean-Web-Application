826d4e503b232df8c3dd16a106c30f6d39de875e
harshadrane67 <harshadrane67@gmail.com>
2022-01-19 22:21:48 +0530

    add Desk and Folders Model in dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from authapp.models import User,MCProfile
+from grievance_data.models import Grievance
+
+class Desk(models.Model):
+    desk_name = models.CharField(max_length=250)
+    desk_mc_user = models.ForeignKey(MCProfile,on_delete=models.CASCADE)
+    desk_gri_files = models.ForeignKey(Grievance,on_delete=models.SET_NULL,null=True)
+
+    def __str__(self) -> str:
+        return self.desk_name
+    
+class Folders(models.Model):
+    folder_name = models.CharField(max_length=250)
+    folder_gri_file = models.ManyToManyField(Grievance)
+
+    def __str__(self) -> str:
+        return self.folder_name
+
 
 # Create your models here.
