3d55634f40efd828cb02694075d47446164cda90
mandharet <tejasm@skiller.com>
2022-01-22 02:08:13 +0530

    create account setup pages

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,6 +14,9 @@ def colorDemo(request):
 def searchDemo(request):
     return render(request,'search-results.html')
 
+def grievance(request):
+    return render(request,'grievance-detail.html')
+
 def signin(request):
     if request.method == 'POST':
         mail = request.POST.get('mail')
@@ -26,7 +29,7 @@ def signin(request):
             
         else:
             return redirect("dashboard:register")
-    return render(request,'signin.html')
+    return render(request,'sign-in.html')
 
 
 
@@ -34,7 +37,6 @@ def register(request):
     if request.method == 'POST':
         firstname = request.POST.get('fname')
         lastname = request.POST.get('lname')
-        username = request.POST.get('username')
         email = request.POST.get('mail')
         username = request.POST.get('mail')
         password = request.POST.get('password')
@@ -43,9 +45,13 @@ def register(request):
             first_name=firstname, last_name=lastname, username=username, email=email, password=password)
         user.save()
         login(request, user)
-        return redirect("dashboard:dashboard")
+        return redirect("dashboard:search")
     
-    return render(request,'register.html')
+    return render(request,'sign-up.html')
 
+def accountDetail(request):
+    return render(request,'account-detail.html')
 
+def accountSetting(request):
+    return render(request,'account-setting.html')
     
