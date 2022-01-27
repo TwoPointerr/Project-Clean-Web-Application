8d294c09b38220d55e171b58d5ed74c3658fe12e
harshadrane67 <harshadrane67@gmail.com>
2022-01-27 17:34:13 +0530

    Solve folder create loop by puting create folder in base .html

--- static/js/CreateFolder.js
+++ static/js/CreateFolder.js
@@ -1,51 +1,51 @@
+$(document).ready(function () {
+  //muncipal
+  $("#create-modal-folder-button").click(function () {
+    var folder_name = $("#modal-folder-name-id").val();
+    var desk_id = $(".folder-modal-add-button").attr("id");
+    console.log(desk_id);
 
-//muncipal 
-$("#create-modal-folder-button").click(function () {
-  var folder_name = $("#modal-folder-name-id").val();
-  var desk_id = $(".folder-modal-add-button").attr("id");
-  console.log(desk_id)
+    data = {};
+    data.folder_name = folder_name;
+    data.desk_id = desk_id;
+
+    $.ajax({
+      url: "/dashboard/create-folder/",
+      data: data,
+      dataType: "json",
+      success: function (res) {
+        var host = window.location.host;
+        console.log(host);
+        $.getScript("../../static/js/RefreshFolderModal.js", function () {});
+        //$("div.modalSelectFolderAJAX").html(res.data);
+      },
+    });
+  });
+});
+
+// worksapce dash board modal
+$("#create-workspace-folder-button").click(function () {
+  var folder_name = $("#workspace-folder-name-id").val();
+  var desk_id = $(".desk-title").attr("id");
+  console.log(desk_id);
 
   data = {};
   data.folder_name = folder_name;
-  data.desk_id = desk_id
-  
+  data.desk_id = desk_id;
 
   $.ajax({
     url: "/dashboard/create-folder/",
     data: data,
     dataType: "json",
     success: function (res) {
-      var host = window.location.host
-      console.log(host)
-      $.getScript("../../static/js/RefreshFolderModal.js",function(){});
+      var host = window.location.host;
+      console.log(host);
+      $.getScript(
+        "../../static/js/DisplayWorkSpaceFolderList.js",
+        function () {}
+      );
       console.log(res);
       //$("div.modalSelectFolderAJAX").html(res.data);
     },
   });
 });
-
-
-// worksapce dash board modal
-// $("#create-workspace-folder-button").click(function () {
-//   var folder_name = $("#workspace-folder-name-id").val();
-//   var desk_id = $(".desk-title").attr("id");
-//   console.log(desk_id)
-
-//   data = {};
-//   data.folder_name = folder_name;
-//   data.desk_id = desk_id
-  
-
-//   $.ajax({
-//     url: "/dashboard/create-folder/",
-//     data: data,
-//     dataType: "json",
-//     success: function (res) {
-//       var host = window.location.host
-//       console.log(host)
-//       $.getScript("../../static/js/DisplayWorkSpaceFolderList.js",function(){});
-//       console.log(res);
-//       //$("div.modalSelectFolderAJAX").html(res.data);
-//     },
-//   });
-// });
