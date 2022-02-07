01782a28afb8d61ec8844b2f38722366a8f08901
harshadrane67 <harshadrane67@gmail.com>
2022-02-07 20:04:53 +0530

    add staticFiles_dirs const in setting

--- project_clean/settings.py
+++ project_clean/settings.py
@@ -169,12 +169,16 @@ LOGIN_URL = reverse_lazy('dashboard:signin')
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    )
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 WHITENOISE_USE_FINDERS = True
 
