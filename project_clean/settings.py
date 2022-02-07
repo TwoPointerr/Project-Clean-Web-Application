b0722b6c31fea7bd397362f856d022a8f83c01b3
harshadrane67 <harshadrane67@gmail.com>
2022-02-07 21:32:48 +0530

    remove static files dir in setting

--- project_clean/settings.py
+++ project_clean/settings.py
@@ -170,9 +170,9 @@ LOGIN_URL = reverse_lazy('dashboard:signin')
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-    )
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+#     )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
