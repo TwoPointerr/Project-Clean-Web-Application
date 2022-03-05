464018d6dbbb65b984530dfc5805aefac15f6240
harshadrane67 <harshadrane67@gmail.com>
2022-03-05 17:31:14 +0530

    created ml function and added ml model

--- grievance_data/models.py
+++ grievance_data/models.py
@@ -2,14 +2,12 @@ from ast import arg
 from django.db import models
 from django.dispatch import receiver
 from authapp.models import CitizenProfile, User, MCProfile, Location
-from django.db.models.signals import post_save
-import datetime
+from django.db.models.signals import post_save, pre_save
 from PIL import Image
-import uuid
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
-import sys
 from django.core.files import File
+from ml_models.ml_functions import gri_priority, gri_severity
 
 class Category(models.Model):
     cat_name = models.CharField(max_length=250)
@@ -22,39 +20,49 @@ class Grievance(models.Model):
     gri_title = models.CharField(max_length=250)
     gri_desc = models.TextField(null=True)
     gri_category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True, related_name='category')
-    #gri_upvote = models.PositiveIntegerField(default=0)
+    gri_upvote = models.PositiveIntegerField(default=0)
     gri_upvote_list = models.ManyToManyField(CitizenProfile)
-    gri_upvote = models.IntegerField(default=0)
+    #gri_upvote = models.IntegerField(default=0)
     gri_severity = models.IntegerField(default=0)
     gri_priority = models.IntegerField(default=0)
     gri_uploaded_user = models.ForeignKey(CitizenProfile,on_delete=models.CASCADE, related_name='uploaded_user')
-    gri_location = models.OneToOneField(Location,on_delete=models.SET_NULL,null=True)
+    gri_location = models.OneToOneField(Location,on_delete=models.CASCADE,null=True)
     gri_timeStamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.gri_title
 
-    # def save(self,*args, **kwargs):
-    #     self.gri_img = self.reduce_image_size(self.gri_img)
-    #     super().save(*args,**kwargs)
+    def save(self,*args, **kwargs):
 
-    def reduce_image_size(self, profile_pic):
-        print(profile_pic)
-        img = Image.open(profile_pic)
-        thumb_io = BytesIO()
-        img.thumbnail((500,668))
-        img.save(thumb_io, "jpeg", quality=50)
-        new_image = File(thumb_io, name=profile_pic.name)
-        return new_image
+        if not self._state.adding:
+            self.gri_priority = gri_priority(self)
+            self.gri_upvote = self.gri_upvote_list.all().count()
+        super().save(*args,**kwargs)
+
+# def reduce_image_size(profile_pic):
+#     print(profile_pic)
+#     img = Image.open(profile_pic)
+#     thumb_io = BytesIO()
+#     img.thumbnail((500,668))
+#     img.save(thumb_io, "jpeg", quality=50)
+#     new_image = File(thumb_io, name=profile_pic.name)
+#     return new_image
+
+# @receiver(pre_save,sender=Grievance,)
+# def pre_save_grievacne(sender, instance, **kwargs):
+#     img = Image.open(instance.gri_img)
+#     thumb_io = BytesIO()
+#     img.thumbnail((500,668))
+#     img.save(thumb_io, "jpeg", quality=50)
+#     instance.gri_img = File(thumb_io, name=instance.gri_img.name)
 
 @receiver(post_save,sender=Grievance,)
-def register_status(sender, instance, **kwargs):
+def post_save_grievacne(sender, instance, **kwargs):
     if kwargs['created']:
         status = Status(status_name="Register",status_grievance=instance,status_issuedByMC=None)
         status.save()
         status = Status(status_name="Pending",status_grievance=instance,status_active=True,status_issuedByMC=None)
         status.save()
-
 
 STATUS_NAME = (
     ("Register","Register"),
